6be94c3898eaa1ddca924e1db33fd95f85329a92
Gagan Chaudhary <singhgaganbtp@gmail.com>
2022-12-31 22:11:23 +0530

    changes made

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let blogEl=document.getElementById("blog-content");
-
-
+const buttonEl=document.getElementById("bt");
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((response) => response.json())
   .then((data) => 
@@ -17,5 +16,39 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     blogEl.innerHTML=str;
 
   });
+
+  buttonEl.addEventListener('click',function(e){
+    e.preventDefault;
+    const postTitle=document.getElementById("post-title").value;
+    const postBody=document.getElementById("post-body").value;
+    const data={
+        title:postTitle,
+        body:postBody
+    }
+    const options={
+        method:"POST",
+        body:JSON.stringify(data),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts",options)
+        .then(res => res.json())
+        .then(data =>console.log(data))
+    })
+ 
+// fetch("https://apis.scrimba.com/jsonplaceholder/todos",{
+//     method:"POST",
+//     body:JSON.stringify({
+//         title:"Buy milk",
+//         completed:false
+//     }),
+//     headers:{
+//         "Content-Type":"application/json"// basically we are teeling server that we are sending json and you have to parse the javascript to me and it gives the database we have plus it gives the id also.
+//     }
+// })
+//     .then(res => res.json())
+//     .then(data => console.log(data))
 
 
